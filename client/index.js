8e1b1b79da3ac6ed1cbd526d74944bea659663b4
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-09-19 14:54:56 -0700

    Fix variable names

--- client/index.js
+++ client/index.js
@@ -2,9 +2,13 @@ window.onload = () => {
   const inner = document.getElementById('inner');
   const tracker = document.getElementById('tracker');
 
+  const delta = 10;
+
+  let innerWidthNum, percentage;
+
   let interval = setInterval(() => {
-    let innerWidth = inner.style.width.slice(0, inner.style.width.length - 1);
-    let percentage = +innerWidth + 10 + '%';
+    innerWidthNum = inner.style.width.slice(0, inner.style.width.length - 1);
+    percentage = +innerWidthNum + delta + '%';
 
     tracker.innerHTML = percentage;
     inner.style.width = percentage;
